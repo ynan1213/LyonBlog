cf0e9bc8a76451abf5136b21a7968c0dde3ab8df
Administrator <Administrator@windows10.microdone.cn>
2019-04-11 19:34:18 +0800

    重新梳理spring IOC 2019.04.11

--- spring_study/src/main/java/com/sqsoft/main/MainTest.java
+++ spring_study/src/main/java/com/sqsoft/main/MainTest.java
@@ -5,8 +5,6 @@ import java.util.Properties;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.sqsoft.service.Service1;
-
 public class MainTest {
 
 	public static void main(String[] args) {
@@ -15,8 +13,6 @@ public class MainTest {
 		properties.put("str", "sqsoft");
 		
 		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("classpath:application.xml");
-		Service1 s1 = ac.getBean("service1",Service1.class);
-		System.out.println(s1);
 		ac.close();
 	}
 }

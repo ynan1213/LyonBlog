1e3c6903e17bf0d5566193163b6b798738b94811
ynan1213 <452245790@qq.com>
2022-02-17 23:04:51 +0800

    消息重试问题

--- spring-cloud-stream/spring-cloud-stream-2.1.0.RELEASE/04-stream-rocketmq/src/main/java/com/ynan/mq/XxxMessageReceiver.java
+++ spring-cloud-stream/spring-cloud-stream-2.1.0.RELEASE/04-stream-rocketmq/src/main/java/com/ynan/mq/XxxMessageReceiver.java
@@ -1,6 +1,8 @@
 package com.ynan.mq;
 
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -13,22 +15,28 @@ import org.springframework.messaging.handler.annotation.Payload;
 @EnableBinding({XxxMessageBinding.class})
 public class XxxMessageReceiver {
 
+    Logger logger = LoggerFactory.getLogger("com.ynan.xx");
+
+    private static int count = 0;
+
     //@Input 被@StreamListener注解的方法不能再被@Input注解了，否则启动时抛异常
     @StreamListener(XxxMessageBinding.INPUT)
     public void receive(@Payload Object payload, @Headers Map headers) {
-        for (Object o : headers.keySet()) {
-            System.out.println(o.toString() + " ：" + headers.get(o).toString());
-        }
-        System.out.println("接收到消息体：" + payload);
+//        for (Object o : headers.keySet()) {
+//            System.out.println(o.toString() + " ：" + headers.get(o).toString());
+//        }
+        System.out.println("接收到消息体：" + payload + "    第" + (count ++) + "次消费");
+        logger.info("   消息id：" + headers.get("rocketmq_MESSAGE_ID"));
+        if(1== 1) throw new RuntimeException("XXX---YYY---ZZZ");
     }
 
     // 同一个value值可以配置在多个方法上，包括可以配置在别的类上
     // 具体见 @see com.ynan.mq.XxxMessageReceiver1
-    @StreamListener(XxxMessageBinding.INPUT)
-    public void receive1(@Payload Object payload, @Headers Map headers) {
-        for (Object o : headers.keySet()) {
-            System.out.println(o.toString() + " ：" + headers.get(o).toString());
-        }
-        System.out.println("111111 接收到消息体：" + payload);
-    }
+//    @StreamListener(XxxMessageBinding.INPUT)
+//    public void receive1(@Payload Object payload, @Headers Map headers) {
+//        for (Object o : headers.keySet()) {
+//            System.out.println(o.toString() + " ：" + headers.get(o).toString());
+//        }
+//        System.out.println("111111 接收到消息体：" + payload);
+//    }
 }
